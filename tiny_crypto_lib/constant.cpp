fd54ecf06305a802c555d38add2813fe3e6154e5
Lee <91820141+leerh123@users.noreply.github.com>
2022-10-30 11:56:49 +0800

    常量和全局函数

--- tiny_crypto_lib/constant.cpp
+++ tiny_crypto_lib/constant.cpp
@@ -1,63 +1,46 @@
+#include "constant.h"
 #include"constant.h"
-//将n_l_的位数设置为a_
-inline void SETDIGITS_L(LINT& n_, lint a_) {
-	n_[0] = a_;
-}
-//获取n_l_的位数
-inline lint DIGITS_L(const LINT& n_) {
-	return n_[0];
-}
-//获取最低有效位的指针
-inline lint* LSDPTR_L(LINT& n_) {
-	if (DIGITS_L(n_) == 0) {
-		return nullptr;
-	}
-	return &n_[1];
-}
-inline const lint* LSDPTR_L(const LINT& n_) {
-	if (DIGITS_L(n_) == 0) {
-		return nullptr;
-	}
-	return &n_[1];
-}
-//获取最高有效位的指针
-inline lint* MSDPTR_L(LINT& n_) {
-	if (DIGITS_L(n_) == 0) {
-		return nullptr;
-	}
-	return &n_[DIGITS_L(n_)];
-}
-inline const lint* MSDPTR_L(const LINT& n_) {
-	if (DIGITS_L(n_) == 0) {
-		return nullptr;
-	}
-	return &n_[DIGITS_L(n_)];
-}
+//错误代码
+extern const short kE_LINT_DBZ = -1;//被零除
+extern const short kE_LINT_OFL = -2;//上溢
+extern const short kE_LINT_UFL = -3;//下溢
+extern const short kE_LINT_MAL = -4;//内存分配错误
+extern const short kE_LINT_NOR = -5;//寄存器不可用
+extern const short kE_LINT_BOR = -6;//基数不合法
+extern const short kE_LINT_MOD = -7;//模数为偶
+extern const short kE_LINT_NPT = -8;//将空指针作为参数传递
+extern const short kE_VCHECK_OFL = 1;//警告：数字过长
+extern const short kE_VCHECK_LDZ = 2;//警告：前导零
+extern const short kE_VCHECK_MEM = -1;//空指针
+
+//常数
+extern const unsigned int kBASE = 0X10000;//基数B
+extern const unsigned int kBASEMINONE = 0XFFFFU;//B - 1
+extern const unsigned long kDBASEMINONE = 0XFFFFFFFFUL;//B^2 - 1
+extern const unsigned int kBASEDIV2 = 0X8000U;//B/2
+extern const unsigned int kNOOFREGS = 16U;//寄存器标准编号
+extern const unsigned int kBITPERDIG = 16U;//每个LINT数字中包含的二进制位数
+extern const unsigned int kLDBITPERDGT = 4U;//以二为底kBITPERDIG的对数
+extern const unsigned int kLINTMAXDIGIT = 256U;//LINT对象以B为基数的最大位数
+extern const unsigned int kLINTMAXSHORT = kLINTMAXDIGIT + 1;//为LINT对象分配的USHORT个数
+extern const unsigned int kLINTMAXBYTE = kLINTMAXSHORT << 1;//为LINT对象分配的字节数
+extern const unsigned int kLINTMAXBIT = kLINTMAXDIGIT << 4;//LINT对象包含的最大二进制位数
+extern const double kFLINT_VERSION = 0.1;//版本号
+extern const std::pair<unsigned int, unsigned int> kLINT_SECURE = { 0X73,0 };//安全模式标识符
+
+
+//函数
+
 //检查n_l_是否是偶数
-inline bool ISEVEN_L(const LINT& n_) {
+bool ISEVEN_L(const LINT& n_) {
 	return (DIGITS_L(n_) == 0) || ((DIGITS_L(n_) > 0) && ((*(LSDPTR_L(n_)) & 1) == 0));
 }
 //检查n_l_是否是奇数
-inline bool ISODD_L(const LINT& n_) {
-	return (DIGITS_L(n_) > 0) && ((*(LSDPTR_L(n_)) & 1) == 1);
-}
-//位数减一
-inline void DECDIGITS_L(LINT& n_) {
-	if (n_[0] == 0)
-		return;
-	n_[0]--;
-}
-//位数加一
-inline void INCDIGITS_L(LINT& n_) {
-	n_[0]++;
-}
-//去除前导零
-inline void RMLDZRS_L(LINT& n_) {
-	while ((DIGITS_L(n_) > 0) && (*(MSDPTR_L(n_)) == 0))
-		DECDIGITS_L(n_);
+bool ISODD_L(const LINT& n_) {
+	return (DIGITS_L(n_) > 0) && (((*(LSDPTR_L(n_))) & 1) == 1);
 }
 //模Nmax+1
-inline void ANDMAX_L(LINT& n_) {
+void ANDMAX_L(LINT& n_) {
 	SETDIGITS_L(n_, std::min(DIGITS_L(n_), (lint)kLINTMAXDIGIT));
 	RMLDZRS_L(n_);
 }
@@ -138,57 +121,64 @@ void u2lint_l(LINT& num_, unsigned short u) {
 	RMLDZRS_L(num_);
 }
 //比较a_>=b_
-inline bool GE_L(const LINT& a_, const LINT& b_) {
+bool GE_L(const LINT& a_, const LINT& b_) {
 	return (cmp_l(a_, b_) > -1);
 }
 //比较a_>b_
-inline bool GT_L(const LINT& a_, const LINT& b_) {
+bool GT_L(const LINT& a_, const LINT& b_) {
 	return (cmp_l(a_, b_) == 1);
 }
 //比较a_<b_
-inline bool LT_L(const LINT& a_, const LINT& b_) {
+bool LT_L(const LINT& a_, const LINT& b_) {
 	return (cmp_l(a_, b_) == -1);
 }
 //比较两个LINT对象的最大值
-inline LINT MAX_L(const LINT& a_, const LINT& b_) {
+LINT MAX_L(const LINT& a_, const LINT& b_) {
 	return (GT_L(a_, b_) ? a_ : b_);
 }
 //比较两个LINT对象的最小值
-inline LINT MIN_L(const LINT& a_, const LINT& b_) {
+LINT MIN_L(const LINT& a_, const LINT& b_) {
 	return (LT_L(a_, b_) ? a_ : b_);
 }
 //比较a_==0
-inline bool EQZ_L(LINT& a_) {
+bool EQZ_L(LINT& a_) {
 	LINT tmp;
 	u2lint_l(tmp, 0);
 	return (equ_l(a_, tmp));
 }
 //比较a_==1
-inline bool EQONE_L(LINT& a_) {
+bool EQONE_L(LINT& a_) {
 	LINT tmp;
 	u2lint_l(tmp, 1);
 	return (equ_l(a_, tmp));
 }
 //比较a_>0
-inline bool GTZ_L(LINT& a_) {
+bool GTZ_L(LINT& a_) {
 	LINT tmp;
 	u2lint_l(tmp, 0);
 	return (GT_L(a_, tmp));
 }
 
 //将a_设为0
-inline void SETZERO_L(LINT& a_) {
+void SETZERO_L(LINT& a_) {
 	a_[0] = 0;
 }
 //将a_设为1
-inline void SETONE_L(LINT& a_) {
+void SETONE_L(LINT& a_) {
 	u2lint_l(a_, 1);
 }
 //将a_设为2
-inline void SETTWO_L(LINT& a_) {
+void SETTWO_L(LINT& a_) {
 	u2lint_l(a_, 2);
 }
 //将a_设为ul
-inline void SET_L(LINT& a_, unsigned long ul) {
+void SET_L(LINT& a_, unsigned long ul) {
 	ul2lint_l(a_, ul);
+}
+//将a_设置为Nmax
+void SETMAX_L(LINT& a_) {
+	SETDIGITS_L(a_, kLINTMAXDIGIT);
+	for (auto begin = a_.begin() + 1; begin != a_.end(); begin++) {
+		*(begin) = USHRT_MAX;
+	}
 }
--- tiny_crypto_lib/constant.h
+++ tiny_crypto_lib/constant.h
@@ -4,63 +4,83 @@
 #include<algorithm>
 
 //错误代码
-const short kE_LINT_DBZ = -1;//被零除
-const short kE_LINT_OFL = -2;//上溢
-const short kE_LINT_UFL = -3;//下溢
-const short kE_LINT_MAL = -4;//内存分配错误
-const short kE_LINT_NOR = -5;//寄存器不可用
-const short kE_LINT_BOR = -6;//基数不合法
-const short kE_LINT_MOD = -7;//模数为偶
-const short kE_LINT_NPT = -8;//将空指针作为参数传递
-const short kE_VCHECK_OFL = 1;//警告：数字过长
-const short kE_VCHECK_LDZ = 2;//警告：前导零
-const short kE_VCHECK_MEM = -1;//空指针
+extern const short kE_LINT_DBZ;//被零除
+extern const short kE_LINT_OFL;//上溢
+extern const short kE_LINT_UFL;//下溢
+extern const short kE_LINT_MAL;//内存分配错误
+extern const short kE_LINT_NOR;//寄存器不可用
+extern const short kE_LINT_BOR;//基数不合法
+extern const short kE_LINT_MOD;//模数为偶
+extern const short kE_LINT_NPT;//将空指针作为参数传递
+extern const short kE_VCHECK_OFL;//警告：数字过长
+extern const short kE_VCHECK_LDZ;//警告：前导零
+extern const short kE_VCHECK_MEM;//空指针
 
 //常数
-const unsigned int kBASE = 0X10000;//基数B
-const unsigned int kBASEMINONE = 0XFFFFU;//B - 1
-const unsigned long kDBASEMINONE = 0XFFFFFFFFUL;//B^2 - 1
-const unsigned int kBASEDIV2 = 0X8000U;//B/2
-const unsigned int kNOOFREGS = 16U;//寄存器标准编号
-const unsigned int kBITPERDIG = 16U;//每个LINT数字中包含的二进制位数
-const unsigned int kLDBITPERDGT = 4U;//以二为底kBITPERDIG的对数
-const unsigned int kLINTMAXDIGIT = 256U;//LINT对象以B为基数的最大位数
-const unsigned int kLINTMAXSHORT = kLINTMAXDIGIT + 1;//为LINT对象分配的USHORT个数
-const unsigned int kLINTMAXBYTE = kLINTMAXSHORT << 1;//为LINT对象分配的字节数
-const unsigned int kLINTMAXBIT = kLINTMAXDIGIT << 4;//LINT对象包含的最大二进制位数
-const double kFLINT_VERSION = 0.1;//版本号
-const std::pair<const unsigned int, const unsigned int> kLINT_SECURE = { 0X73,0 };//安全模式标识符
+extern const unsigned int kBASE;//基数B
+extern const unsigned int kBASEMINONE;//B - 1
+extern const unsigned long kDBASEMINONE;//B^2 - 1
+extern const unsigned int kBASEDIV2;//B/2
+extern const unsigned int kNOOFREGS;//寄存器标准编号
+extern const unsigned int kBITPERDIG;//每个LINT数字中包含的二进制位数
+extern const unsigned int kLDBITPERDGT;//以二为底kBITPERDIG的对数
+extern const unsigned int kLINTMAXDIGIT;//LINT对象以B为基数的最大位数
+extern const unsigned int kLINTMAXSHORT;//为LINT对象分配的USHORT个数
+extern const unsigned int kLINTMAXBYTE;//为LINT对象分配的字节数
+extern const unsigned int kLINTMAXBIT;//LINT对象包含的最大二进制位数
+extern const double kFLINT_VERSION;//版本号
+extern const std::pair<unsigned int, unsigned int> kLINT_SECURE;//安全模式标识符
 
 //类型
 using lint = unsigned short;
-using LINT = std::array<lint, kLINTMAXDIGIT + 1>;
-using LINTD = std::array<lint, (kLINTMAXDIGIT << 1) + 1>;
-using LINTQ = std::array<lint, (kLINTMAXDIGIT << 2) + 1>;
+using LINT = std::array<lint, 257>;//kLINTMAXDIGIT + 1
+using LINTD = std::array<lint, 513>;//(kLINTMAXDIGIT << 1) + 1
+using LINTQ = std::array<lint, 1025>;//(kLINTMAXDIGIT << 2) + 1
 
 //函数
-
 //将n_l_的位数设置为a_
-inline void SETDIGITS_L(LINT& n_, lint a_);
-// 获取n_l_的位数
-inline lint DIGITS_L(const LINT & n_);
+inline void SETDIGITS_L(LINT& n_, lint a_) {
+	n_[0] = a_;
+}
+//获取n_l_的位数
+inline lint DIGITS_L(const LINT& n_) {
+	return n_[0];
+}
 //获取最低有效位的指针
-inline lint* LSDPTR_L(LINT& n_);
-inline const lint* LSDPTR_L(const LINT& n_);
+inline lint* LSDPTR_L(LINT& n_) {
+	return &n_[1];
+}
+inline const lint* LSDPTR_L(const LINT& n_) {
+	return &n_[1];
+}
 //获取最高有效位的指针
-inline lint* MSDPTR_L(LINT& n_);
-inline const lint* MSDPTR_L(const LINT& n_);
-//检查n_l_是否是偶数
-inline bool ISEVEN_L(const LINT& n_);
-//检查n_l_是否是奇数
-inline bool ISODD_L(const LINT& n_);
+inline lint* MSDPTR_L(LINT& n_) {
+	return &n_[DIGITS_L(n_)];
+}
+inline const lint* MSDPTR_L(const LINT& n_) {
+	return &n_[DIGITS_L(n_)];
+}
 //位数减一
-inline void DECDIGITS_L(LINT& n_);
+inline void DECDIGITS_L(LINT& n_) {
+	if (n_[0] == 0)
+		return;
+	n_[0]--;
+}
 //位数加一
-inline void INCDIGITS_L(LINT& n_);
-// 去除前导零
-inline void RMLDZRS_L(LINT & n_);
+inline void INCDIGITS_L(LINT& n_) {
+	n_[0]++;
+}
+//去除前导零
+inline void RMLDZRS_L(LINT& n_) {
+	while ((DIGITS_L(n_) > 0) && (*(MSDPTR_L(n_)) == 0))
+		DECDIGITS_L(n_);
+}
+//检查n_l_是否是偶数
+bool ISEVEN_L(const LINT& n_);
+//检查n_l_是否是奇数
+bool ISODD_L(const LINT& n_);
 //模Nmax+1
-inline void ANDMAX_L(LINT& n_);
+void ANDMAX_L(LINT& n_);
 //比较两个LINT对象是否相等
 bool equ_l(const LINT& a_, const LINT& b_);
 //比较两个LINT对象(a_>b_->1; a_<b_->-1; a_==b_->0)
@@ -72,26 +92,30 @@ void ul2lint_l(LINT& num_, unsigned long ul);
 //将一个unsigned short 类型的数字转换为LINT类型对象
 void u2lint_l(LINT& num_, unsigned short u);
 //比较a_>=b_
-inline bool GE_L(const LINT& a_, const LINT& b_);
+bool GE_L(const LINT& a_, const LINT& b_);
 //比较a_>b_
-inline bool GT_L(const LINT& a_, const LINT& b_);
+bool GT_L(const LINT& a_, const LINT& b_);
 //比较a_<b_
-inline bool LT_L(const LINT& a_, const LINT& b_);
+bool LT_L(const LINT& a_, const LINT& b_);
 //比较两个LINT对象的最大值
-inline LINT MAX_L(const LINT& a_, const LINT& b_);
+LINT MAX_L(const LINT& a_, const LINT& b_);
 //比较两个LINT对象的最小值
-inline LINT MIN_L(const LINT& a_, const LINT& b_);
+LINT MIN_L(const LINT& a_, const LINT& b_);
 //比较a_==0
-inline bool EQZ_L(LINT& a_);
+bool EQZ_L(LINT& a_);
 //比较a_==1
-inline bool EQONE_L(LINT& a_);
+bool EQONE_L(LINT& a_);
 //比较a_>0
-inline bool GTZ_L(LINT& a_);
+bool GTZ_L(LINT& a_);
 //将a_设为0
-inline void SETZERO_L(LINT& a_);
+void SETZERO_L(LINT& a_);
 //将a_设为1
-inline void SETONE_L(LINT& a_);
+void SETONE_L(LINT& a_);
 //将a_设为2
-inline void SETTWO_L(LINT& a_);
+void SETTWO_L(LINT& a_);
 //将a_设为ul
-inline void SET_L(LINT& a_, unsigned long ul);
+void SET_L(LINT& a_, unsigned long ul);
+//将a_设置为Nmax
+void SETMAX_L(LINT& a_);
+
+
